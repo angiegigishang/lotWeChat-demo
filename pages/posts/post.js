3c8ea7f8b26b9685e694fc85bd1d132120da37a3
angiegigishang <shangxingyu@163.com>
2018-12-12 21:28:11 +0800

    begin update

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,18 +1,31 @@
 // pages/posts/post.js
+var postsData = require('../../data/posts-data.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    date: "Dec 13 2018",
+    title: "5G频谱划分尘埃落定 智慧城市应用风云再起"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+     //服务器动态获取数据
+     var post_content1 = {
+       date: "Dec 13 2018",
+       title: "5G频谱划分尘埃落定 智慧城市应用风云再起",
+       img: {
+         post_img: "/images/swiper1.jpg",
+         author_img: "/images/logo.jpg"
+       },      
+       content: "工业和信息化部2018年12月10日发文表示，5G系统中低频段试验频率使用许可已经发放。下一步，工信部将积极指导各基础电信运营企业做好5G系统试验的基站部署，促进我国5G产业的健康快速发展...",     
+     }
+     this.setData(post_content1)
   },
 
   /**
